2cf22d8bf408314f6ca3dffdd71dc40f1a09765e
andrew.freelance <andrew.freelance@i.ua>
2015-07-14 23:33:22 +0300

    ...

--- ffmpeg-tutorial0/tutorial05.c
+++ ffmpeg-tutorial0/tutorial05.c
@@ -21,6 +21,10 @@
 #include <libavutil/avstring.h>
 #include <libavutil/time.h>
 
+#include <pthread.h>
+#include <windows.h>
+#include <semaphore.h>  /* Semaphore */
+
 #ifdef __RESAMPLER__
 #include <libavutil/opt.h>
 
@@ -127,6 +131,7 @@ typedef struct VideoState {
 } VideoState;
 
 SDL_Surface     *screen;
+pthread_t thread;
 
 /* Since we only have one decoding thread, the Big Struct
    can be global in case we need it. */
@@ -1076,14 +1081,95 @@ fail: {
     return 0;
 }
 
+const char g_szClassName[] = "myWindowClass";
+
+// Step 4: the Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg)
+    {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+        break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+sem_t mutex;
+HWND hwnd;
+
+static void * window_manager(void * data){
+	printf("Window manager has been started!\n");
+	WNDCLASSEX wc;
+    
+    MSG Msg;
+
+    //Step 1: Registering the Window Class
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.cbClsExtra    = 0;
+    wc.cbWndExtra    = 0;
+    //wc.hInstance     = hInstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszMenuName  = NULL;
+    wc.lpszClassName = g_szClassName;
+    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+
+    if(!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, "Window Registration Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    // Step 2: Creating the Window
+    hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        g_szClassName,
+        "The title of my window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+        NULL, NULL, NULL, NULL);
+	sem_post(&mutex);
+    if(hwnd == NULL)
+    {
+        MessageBox(NULL, "Window Creation Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    } else {
+		printf("window hwnd = %d\n", hwnd);
+	}
+
+    ShowWindow(hwnd, TRUE);
+    UpdateWindow(hwnd);
+
+    // Step 3: The Message Loop
+    while(GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+	
+    return Msg.wParam;
+}
+
 int main(int argc, char *argv[]) {
 
     SDL_Event       event;
 
     VideoState      *is;
 	
-	char * parent_window_arg = NULL;
 	unsigned int parent_window = 0;
+	sem_init(&mutex, 0, 0);
+
 	printf("start!\n"); 
 
     is = av_mallocz(sizeof(VideoState));
@@ -1114,12 +1200,14 @@ int main(int argc, char *argv[]) {
     }
 	printf("********************");
 	fflush(stdout);
-	if (argc == 3) {
-		parent_window_arg = argv[2];
-		parent_window = atoi(parent_window_arg);
-		printf("parent_window arg = %s, descriptor is %d\n", parent_window_arg, parent_window);
-	}
-
+	
+	pthread_create(&thread, NULL, window_manager, NULL);
+	
+	printf("main window handler = %d\n", hwnd);
+	
+	sem_wait(&mutex);
+		
+	
     av_strlcpy(is->filename, argv[1], 1024);
 
     is->pictq_mutex = SDL_CreateMutex();
@@ -1165,7 +1253,7 @@ int main(int argc, char *argv[]) {
                 break;
         }
     }
-
+	pthread_join(thread, NULL);
     return 0;
 
 }
