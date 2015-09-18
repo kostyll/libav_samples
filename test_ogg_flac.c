04fe2b2a176a7500f78a15c8aa93ccff330d6cf3
Andrew <andrew.freelance@i.ua>
2015-09-18 11:01:57 -0400

    some progress with ogg(flac + theora)

--- test_ogg_flac.c
+++ test_ogg_flac.c
@@ -4,93 +4,148 @@
 #include <libavcodec/avcodec.h>
 #include <libavutil/avutil.h>
 #include <libswscale/swscale.h>
+#include <libavutil/frame.h>
 
 #include "general.h"
 
 AVCodecContext * add_video_output(AVFormatContext * fmt_ctx, AVCodecContext * cctx, char *outfile){
 
-	AVStream * stream = NULL;
+    AVStream * stream = NULL;
 
-	AVCodec * vcodec = NULL;
-	AVCodecContext * vcodec_ctx = NULL;
+    AVCodec * vcodec = NULL;
+    AVCodecContext * vcodec_ctx = NULL;
 
-	vcodec = avcodec_find_encoder(AV_CODEC_ID_THEORA);
-	if (vcodec == NULL) die("Cannot find video encoder THEORA\n");
+    vcodec = avcodec_find_encoder(AV_CODEC_ID_THEORA);
+    if (vcodec == NULL) die("Cannot find video encoder THEORA\n");
 
-	stream = avformat_new_stream(fmt_ctx, vcodec);
+    stream = avformat_new_stream(fmt_ctx, vcodec);
 
-	vcodec_ctx = stream->codec;
+    vcodec_ctx = stream->codec;
 
-	vcodec_ctx->codec_id = AV_CODEC_ID_THEORA;
-	vcodec_ctx->codec_type = AVMEDIA_TYPE_VIDEO;
-	vcodec_ctx->bit_rate = 1000000;
-	vcodec_ctx->width = 800;
-	vcodec_ctx->height = 600;
-	vcodec_ctx->time_base = (AVRational){1, 25};
-	vcodec_ctx->pix_fmt = AV_PIX_FMT_YUV420P;
+    vcodec_ctx->codec_id = AV_CODEC_ID_THEORA;
+    vcodec_ctx->codec_type = AVMEDIA_TYPE_VIDEO;
+    vcodec_ctx->bit_rate = 1000000;
+    vcodec_ctx->width = 800;
+    vcodec_ctx->height = 600;
+    vcodec_ctx->time_base = (AVRational){1, 25};
+    vcodec_ctx->pix_fmt = AV_PIX_FMT_YUV420P;
 
-	if (avcodec_open2(vcodec_ctx, vcodec, NULL) < 0) die("Cannot open THEORA encoder\n");
+    if (avcodec_open2(vcodec_ctx, vcodec, NULL) < 0) die("Cannot open THEORA encoder\n");
 
-	return vcodec_ctx;
+    return vcodec_ctx;
 }
 
 AVCodecContext * add_audio_output(AVFormatContext * fmt_ctx, AVCodecContext * cctx, char *outfile){
-	AVStream * stream = NULL;
+    AVStream * stream = NULL;
 
-	AVCodec * acodec = NULL;
-	AVCodecContext * acodec_ctx = NULL;
+    AVCodec * acodec = NULL;
+    AVCodecContext * acodec_ctx = NULL;
 
-	acodec = avcodec_find_encoder(AV_CODEC_ID_FLAC);
-	if (acodec == NULL) die("Cannot find video encoder FLAC\n");
+    acodec = avcodec_find_encoder(AV_CODEC_ID_FLAC);
+    if (acodec == NULL) die("Cannot find video encoder FLAC\n");
 
-	stream = avformat_new_stream(fmt_ctx, acodec);
-	acodec_ctx = stream->codec;
+    stream = avformat_new_stream(fmt_ctx, acodec);
+    acodec_ctx = stream->codec;
 
-	acodec_ctx->sample_fmt = AV_SAMPLE_FMT_S32;
-	acodec_ctx->codec_type = AVMEDIA_TYPE_AUDIO;
-	acodec_ctx->codec_id = AV_CODEC_ID_FLAC;
-	acodec_ctx->bit_rate = 64000;
-	acodec_ctx->sample_rate = 48000;
-	acodec_ctx->channels = 2;
+    acodec_ctx->sample_fmt = AV_SAMPLE_FMT_S32;
+    acodec_ctx->codec_type = AVMEDIA_TYPE_AUDIO;
+    acodec_ctx->codec_id = AV_CODEC_ID_FLAC;
+    acodec_ctx->bit_rate = 64000;
+    acodec_ctx->sample_rate = 48000;
+    acodec_ctx->channels = 2;
 
-	if (avcodec_open2(acodec_ctx, acodec, NULL) < 0) die("Cannot open FLAC encoder\n");
+    if (avcodec_open2(acodec_ctx, acodec, NULL) < 0) die("Cannot open FLAC encoder\n");
 
-	return acodec_ctx;
+    return acodec_ctx;
 }
 
 
 int main(int argc, char ** argv){
-	if (argc != 3){
-		fprintf(stderr, "Usage %s <infile> <outfile>\n", argv[0]);
-		return 0;
-	}
-	char * outfile = NULL;
-	outfile = argv[2];
+    if (argc != 3){
+        fprintf(stderr, "Usage %s <infile> <outfile>\n", argv[0]);
+        return 0;
+    }
+    char * outfile = NULL;
+    outfile = argv[2];
 
-	// Initialization
-	av_register_all();
-	avformat_network_init();
+    // Initialization
+    av_register_all();
+    avformat_network_init();
 
-	//Opening input source
-	InputSource * source = NULL;
-	source = open_source(argv[1], 1, 1);
+    //Opening input source
+    InputSource * source = NULL;
+    source = open_source(argv[1], 1, 1);
 
-	//Creating output
-	Output * output;
-	output = open_output(
-		outfile,
-		add_video_output,
-		add_audio_output,
-		NULL,
-		1,
-		1
-	);
-	av_dump_format(output->ctx, 0, outfile, 1);
+    //Creating output
+    Output * output;
+    output = open_output(
+        outfile,
+        add_video_output,
+        add_audio_output,
+        NULL,
+        1,
+        1
+    );
+    av_dump_format(output->ctx, 0, outfile, 1);
 
-	avformat_write_header(output->ctx, NULL);
+    avformat_write_header(output->ctx, NULL);
 
+    struct SwrContext * swr_ctx = NULL;
+    swr_ctx = build_audio_swr(source->actx, output->actx);
 
-	// struct SwrContext * swr_ctx = build_audio_swr()
+    AVPacket packet;
+    AVPacket packet_copy;
+    AVPacket target_packet;
 
-	return 0;
+    AVFrame* frame = NULL;
+    AVFrame* aframe = NULL;
+    AVFrame* daframe = NULL;
+
+    int nb_samples;
+
+    frame = av_frame_alloc();
+    if (frame == NULL) die("Cannot allocate frame\n");
+
+    aframe = alloc_audio_frame(
+        source->actx->sample_fmt,
+        source->actx->channel_layout,
+        source->actx->sample_rate,
+        10000,
+        source->actx->channels
+        );
+    if (aframe == NULL) die("Cannot allocate aframe\n");
+
+    daframe = alloc_audio_frame(
+        output->actx->sample_fmt,
+        output->actx->channel_layout,
+        output->actx->sample_rate,
+        output->ac->capabilities & CODEC_CAP_VARIABLE_FRAME_SIZE ? 10000 : output->actx->frame_size,
+        output->actx->channels
+        );
+    if (daframe == NULL) die("Cannot allocate daframe\n");
+
+    daframe->nb_samples = output->actx->frame_size;
+    daframe->format = output->actx->sample_fmt;
+    daframe->channel_layout = output->actx->channel_layout;
+    daframe->channels = output->actx->channels;
+    daframe->sample_rate = output->actx->sample_rate;
+
+    while(av_read_frame(source->ctx, &packet) >= 0){
+        fprintf(stdout, "packet.pts = %d\n", packet.pts);
+        av_free_packet(&packet);
+    }
+
+    av_write_trailer(output->ctx);
+
+    av_free(frame);
+    av_free(aframe);
+    av_free(daframe);
+
+    avformat_close_input(source->ctx);
+    avformat_close_input(output->ctx);
+
+    swr_close(swr_ctx);
+    swr_free(&swr_ctx);
+
+    return 0;
 }
