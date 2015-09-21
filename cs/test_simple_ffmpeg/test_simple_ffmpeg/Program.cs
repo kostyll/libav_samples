3727814a41602c9fcd7b5d68f2683373465fd062
Andrew <andrew.freelance@i.ua>
2015-09-21 07:55:24 -0400

    some progress with binding lib to c#

---
+++ cs/test_simple_ffmpeg/test_simple_ffmpeg/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace test_simple_ffmpeg
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			simple_ffmpeg sff = new simple_ffmpeg();
+
+			simple_ffmpeg.sff_register_all ();
+			simple_ffmpeg.sff_network_init ();
+
+			string infile = "big_buck_bunny_720p_2mb.mp4";
+
+			SourceOrDestinatio source = simple_ffmpeg.open_source(infile, 1, 1);
+
+			//simple_ffmpeg.sff_dump_format (source, infile);
+
+		}
+	}
+}
