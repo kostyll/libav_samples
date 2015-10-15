1dace484266d624a29cf8af653845eeacca9c064
andrew <andrew.freelance@i.ua>
2015-10-15 21:15:37 +0300

    script for MXE environment was added. hard local dependencies are set

---
+++ env_shared.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PATH=/home/andrew/workspace/libav_samples/save_sss/mxe/usr/bin:$PATH
+export PKG_CONFIG_PATH_i686_w64_mingw32_shared=/home/andrew/workspace/libav_samples/save_sss/mxe/usr/i686-w64-mingw32.PKG_CONFIG_PATH_i686_w64_mingw32_shared
+
+#cmake -DCMAKE_TOOLCHAIN_FILE=save_sss/mxe/usr/i686-w64-mingw32.shared/share/cmake/mxe-conf.cmake .
+
+bash
